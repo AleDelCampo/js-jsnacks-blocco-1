11e39c96b300bdb64904e4362ed55f76bb415757
AleHashirama <alehashirama@gmail.com>
2024-02-02 15:22:27 +0100

    Snack 3 completo (senza css)

---
+++ js-snack-3/script.js
@@ -0,0 +1,16 @@
+const myArray = [];
+
+for (let i = 0; i < "6"; i++) {
+    let userNumbers;
+
+    userNumbers = parseInt(prompt("Benvenuto!! Potresti fornirmi i tuoi 6 numeri dispari??"));
+
+    if(userNumbers % 2 == 0) {
+        document.getElementById("even").innerHTML = "Questi non mi servono ti ho detto!!"
+    } else {
+        myArray.push(`${userNumbers}`)
+    }
+   
+    document.getElementById("numbers").innerHTML = `${myArray}`;
+};
+
